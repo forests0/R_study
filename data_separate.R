6caa65196eafbd122c30ab766c58b69a3db66539
Chell2358 <forestser00@gmail.com>
2023-05-03 08:09:10 +0900

    데이터 구분 추가 공부

--- data_separate.R
+++ data_separate.R
@@ -11,11 +11,12 @@ fpred = predict(f, iris)
 confusionMatrix(rpred, iris$Species) # rpart로 정확률 측정한 것. 행열이 모두 똑같은 것이 정답.
 confusionMatrix(fpred, iris$Species) # randomforest 로 한 것, 정확률은 randomforest가 더 높음
 
-#그렇다면 iris 데이터를 가공할 때에는 randomforests를 사용하는 것이 좋음.
+#그렇다면 iris 데이터를 가공할 때에는 randomforest를 사용하는 것이 좋음.
 
 n = nrow(iris)
 i = i:n
-trainlist = sample(i, n * 0.6) # 60% 를 학습 데이터로 넣고, 랜덤하게 섞음
+trainlist = sample(i, n * 0.7) # 60% 를 학습 데이터로 넣고, 랜덤하게 섞음
+# 60 -> 70으로 수정했는데, 정확도가 조금 오르는 것을 보면 7 : 3 이 가장 최선의 결과를 내기에 좋은 것 같음.
 testlist = setdiff(i, trainlist) # 나머지를 테스트 데이터로 넣기
 iris_train = iris[trainlist, ] # 훈련 집합 추출
 iris_test = iris[testlist, ] # 테스트 집합 추출
