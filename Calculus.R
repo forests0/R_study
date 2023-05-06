732b7831f54598db2c8066f6d8a7bd0d464f5e67
Chell2358 <forestser00@gmail.com>
2023-05-06 10:31:20 +0900

    미적분 공부

---
+++ Calculus.R
@@ -0,0 +1,17 @@
+f = function(x) {
+  return(x^2)
+}
+# 미분 전 함수
+f <- function(x) (x^3 + 3 * x^2 - 6 * x + 8)
+curve(f, -5,  4, ylab = 'y = f(x)')
+
+#미분 후 함수변화
+g <- function(x) {}
+body(g) = D(body(f), 'x')
+curve(g, -5, 4, ylab = 'g(x)') # 3차함수가 미분되어 2차함수로 변한 것을 알 수 있음
+
+# integrate()로 적분해보기
+f2 = function(x) (x^2 + 1)
+integrate(f2, lower = 0, upper = 1)
+integrate(g, lower = 0, upper = 10)
+f(10) - f(0)
