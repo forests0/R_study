cf73011250980821fdb6d0eabcd003466adcc022
Chell2358 <forestser00@gmail.com>
2023-05-09 08:10:15 +0900

    미적분 예시 뉴턴-랩스 방법

--- Calculus.R
+++ Calculus.R
@@ -15,3 +15,23 @@ f2 = function(x) (x^2 + 1)
 integrate(f2, lower = 0, upper = 1)
 integrate(g, lower = 0, upper = 10)
 f(10) - f(0)
+
+newton = function(f, tol = 1e-7, x0 = 1, N = 100) {
+  h = 1e-7;
+  i = 1
+  x1 = x0;
+  p = numeric(N)
+  while(i <= N) {
+    df.dx = (f(x0+h) - f(x0)) / h
+    p[i] = x1
+    i = i + 1
+    if(abs(x1 - x0) < tol) break
+    x0 = x1
+  }
+  return(p[1 : (i - 1)])
+}
+
+f <- function(x) (x^2 - 2)
+newton(f2)
+f <- function(x) (x^3 + 3 * x^2 - 6 * x -8)
+newton(f3)
