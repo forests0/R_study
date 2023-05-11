5a2027f45215bd7a107cddd2e7b73ed2a09e06ea
Chell2358 <forestser00@gmail.com>
2023-05-12 08:10:11 +0900

    /

--- babynames.R
+++ babynames.R
@@ -7,14 +7,12 @@ babys = read.csv("C:/Users/NGY/Desktop/2023/Rstudy/Popular_Baby_Names.csv")
 babys
 
 summary(babys)
-sort(babys)
 class(babys)
 # R은 order이라는 자체 정렬함수를 가지고있다. 혹은 dpylr에 있는 arrange 함수로 하면 될듯
 nbabys = babys[ order(babys$Rank, decreasing = TRUE), ]
+#plot(nbabys)
 
-plot(nbabys)
+ggplot(nbabys, aes(x = Ethnicity, y = Count, col = Gender)) + geom_point()
+ggplot(nbabys, aes(x = Rank, y = Year.of.Birth, col = Gender)) + geom_point()
 
-help('gapminder')
-??gapminder
-
-nbabys %>% filter(Year.of.Birth == 2015) %>% ggplot(nbabys, aes(x = Year.of.Birth, y = Count, col = Gender)) + geom_point()
+is.na(nbabys)
