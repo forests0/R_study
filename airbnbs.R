842b47bdf58bb77cbe1ab7b932477029c8618c8a
Chell2358 <forestser00@gmail.com>
2023-05-22 08:28:30 +0900

    plot에 설명 붙이기

--- airbnbs.R
+++ airbnbs.R
@@ -3,7 +3,7 @@ library(ggplot2)
 
 airbnb = read.csv("C:/Users/NGY/Desktop/2023/Rstudy/Listings.csv")
 head(airbnb, 5)
-#결측치 확인 / 가공공
+#결측치 확인 / 가공
 colSums(is.na(airbnb))
 airbnb$bedrooms[is.na(airbnb$bedrooms)] <- 0
 airbnb$host_total_listings_count[is.na(airbnb$host_total_listings_count)] <- 0
@@ -16,5 +16,9 @@ summary(airbnb)
 filtered_bedroom <- airbnb %>% filter(bedrooms > 5)
 
 ggplot(airbnb) + geom_point(aes(x = review_scores_rating , y = price, col = room_type), size = 1.5, shape = 16, alpha = 0.5)
+#침대 종류에 따른 개수를 보기 위함함
 ggplot(airbnb, aes(x = bedrooms, col = room_type)) + geom_histogram()
+#리뷰한 평점에 대한 신뢰도 검사, 결측치 재가공 전이나 후나 그렇게 큰 차이는 없어보임
 ggplot(airbnb, aes(x =review_scores_rating, y = review_scores_accuracy)) + geom_point()
+#위도, 경도에 따른 위치에 대한 평점이 궁금해서, 물론 지형이나 다른 점을 고려할 게 너무 많지만 주어진 정보가 이게 끝이라서
+ggplot(airbnb, aes(x = review_scores_location, y = latitude, col = longitude)) + geom_point(alpha = 0.5)
