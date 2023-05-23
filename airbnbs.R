5e555baa20348bc03bd78dbb90d96d04ef9dad6e
Chell2358 <forestser00@gmail.com>
2023-05-24 08:02:56 +0900

    분석 추가

--- airbnbs.R
+++ airbnbs.R
@@ -20,5 +20,8 @@ ggplot(airbnb) + geom_point(aes(x = review_scores_rating , y = price, col = room
 ggplot(airbnb, aes(x = bedrooms, col = room_type)) + geom_histogram()
 #리뷰한 평점에 대한 신뢰도 검사, 결측치 재가공 전이나 후나 그렇게 큰 차이는 없어보임
 ggplot(airbnb, aes(x =review_scores_rating, y = review_scores_accuracy)) + geom_point()
-#위도, 경도에 따른 위치에 대한 평점이 궁금해서, 물론 지형이나 다른 점을 고려할 게 너무 많지만 주어진 정보가 이게 끝이라서
+#위도, 경도에 따른 위치에 대한 평점이 궁금해서, 물론 지형이나 다른 점을 고려할 게 너무 많지만 주어진 정보가 이게 끝이라서 아쉽긴함.
 ggplot(airbnb, aes(x = review_scores_location, y = latitude, col = longitude)) + geom_point(alpha = 0.5)
+#깔끔한 정도가 수용 인원에 영향을 받을까? 일단 room_type으로도 구분해서 본다. 물론 Entire place의 수가 매우 많음을 알고 가야하지만
+ggplot(airbnb, aes(x = accommodates, y = review_scores_cleanliness, col = room_type)) + geom_point(alpha = 0.2)
+ggplot(airbnb, aes(x = ))
