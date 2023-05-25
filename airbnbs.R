49ea05782aa2d86abfe44909cce7895f05c0e6a5
Chell2358 <forestser00@gmail.com>
2023-05-26 08:03:30 +0900

    /

--- airbnbs.R
+++ airbnbs.R
@@ -1,6 +1,6 @@
 library(dplyr)
 library(ggplot2)
-
+# Ctrl + L 이면 console Clear
 airbnb = read.csv("C:/Users/NGY/Desktop/2023/Rstudy/Listings.csv")
 head(airbnb, 5)
 #결측치 확인 / 가공
@@ -22,6 +22,12 @@ ggplot(airbnb, aes(x = bedrooms, col = room_type)) + geom_histogram()
 ggplot(airbnb, aes(x =review_scores_rating, y = review_scores_accuracy)) + geom_point()
 #위도, 경도에 따른 위치에 대한 평점이 궁금해서, 물론 지형이나 다른 점을 고려할 게 너무 많지만 주어진 정보가 이게 끝이라서 아쉽긴함.
 ggplot(airbnb, aes(x = review_scores_location, y = latitude, col = longitude)) + geom_point(alpha = 0.5)
-#깔끔한 정도가 수용 인원에 영향을 받을까? 일단 room_type으로도 구분해서 본다. 물론 Entire place의 수가 매우 많음을 알고 가야하지만
+#청소에 관한 점수가 수용 인원에 영향을 받을까? 일단 room_type으로도 구분해서 본다. 물론 Entire place의 수가 매우 많음을 알고 가야하지만
 ggplot(airbnb, aes(x = accommodates, y = review_scores_cleanliness, col = room_type)) + geom_point(alpha = 0.2)
+#가격에 따른 리뷰 점수를 히트맵으로 표현해보려 했는데 count 해결을 해야함. <- 그대로 두는 것이 최선인듯.
+# 수정할 수 있는 방법이 있지만 그러면 표가 크게 흐트러짐.
+ggplot(airbnb, aes(x = price, y = review_scores_rating)) + xlim(1,55000) +geom_bin_2d(bins = 100) + scale_fill_gradient(low = "lightblue", high = "blue")
+#도시에 따른 리뷰 점수 분포도가 궁금함. 보니 홍콩의 평균이 가장 낮음.
+ggplot(airbnb, aes(x = city, y = review_scores_rating)) + geom_boxplot() + xlab('review_score') 
 ggplot(airbnb, aes(x = ))
+
