f879f214c32c39b69d094ba71717aa6988a3fb11
Chell2358 <forestser00@gmail.com>
2023-06-26 11:03:15 +0900

    .

--- google_stock.R
+++ google_stock.R
@@ -12,6 +12,7 @@ stock_weekly$Date <- as.Date(stock_weekly$Date)
 ggplot(stock_monthly, aes(x = High, y =Price, col = Date)) + geom_point()
 #같은 논지로 최소 가격 또한 올라가는중
 ggplot(stock_monthly, aes(x = Low, y =Price, col = Date)) + geom_point()
-#가격대 변화를 boxplot으로 체크0
+#가격대 변화를 boxplot으로 체크
 ggplot(stock_monthly, aes(x = Price)) + geom_boxplot()
 
+ggplot(stock_monthly, aes(x = Date, y = Volume)) + geom_violin() + scale_y_log10()
