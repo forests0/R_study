7d4b1a4b666ef532c5ac6181642d87596d2f5469
Chell2358 <forestser00@gmail.com>
2023-06-29 21:43:33 +0900

    .

--- google_stock.R
+++ google_stock.R
@@ -16,4 +16,6 @@ ggplot(stock_monthly, aes(x = Low, y =Price, col = Date)) + geom_point()
 ggplot(stock_monthly, aes(x = Price)) + geom_boxplot()
 #volume(총 거래량으로 추측) 을 Date와 같이 violin으로 보는중
 ggplot(stock_monthly, aes(x = Date, y = Volume)) + geom_violin() + scale_y_log10()
+#그렇다면 데이터들을 2020 이후로 줄여서 본다면 어떨까?
 filtered_dates <- stock_monthly[stock_monthly$Date >= as.Date("2020-01-01")]
+ggplot(stock_monthly, aes(x = Low, y =Price, col = filtered_dates)) + geom_point()
