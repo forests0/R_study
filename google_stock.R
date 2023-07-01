e87dfb416db64b5b76ff74fe972c67ba341eb717
Chell2358 <forestser00@gmail.com>
2023-07-01 22:58:14 +0900

    .

--- google_stock.R
+++ google_stock.R
@@ -17,8 +17,14 @@ ggplot(stock_monthly, aes(x = Price)) + geom_boxplot()
 #volume(총 거래량으로 추측) 을 Date와 같이 violin으로 보는중
 ggplot(stock_monthly, aes(x = Date, y = Volume)) + geom_violin() + scale_y_log10()
 #그렇다면 데이터들을 2020 이후로 줄여서 본다면 어떨까?
-stock_monthly %>% filter(Date >= as.Date("2020-01-01")) %>% ggplot(aes(x = Low, y =Price, col = Date)) + geom_point()
+stock_monthly %>% filter(Date >= as.Date("2020-01-01")) %>% ggplot(aes(x = Date, y =Price, col = High)) + geom_point()
 #x를 Date로 해놓은 뒤, 가격을 col에 놓는 것이 내가 원하는 결과
-stock_monthly %>% filter(Date >= as.Date("2020-01-01")) %>% ggplot(aes(x = Date, y =Price, col = Price)) + geom_point()
-
+stock_monthly %>% filter(Date >= as.Date("2020-01-01")) %>% ggplot(aes(x = Date, y =Price, col = Low)) + geom_point()
+#이렇게 보니 2022년 코로나로 인한 급격한 상승 이후로 감소세를 보이는 것을 알 수 있음.
+ggplot(stock_monthly, aes(x = Date, y = Close, col = High)) + geom_point() + geom_abline(intercept = 0, slope = 1, color = "red")
+# 2018-2020년대(비트코인 등이 흥했을 시절)에는 어떤 변화가 있을까
+stock_monthly %>% filter(Date >= as.Date('2018-01-01') & Date <= as.Date('2020-01-01')) %>% ggplot(aes(x = Date, y = Close, col = High)) + geom_point()
+#그렇다면 2020년 이전의 양들을 보자.
+stock_monthly %>% filter(Date <= as.Date('2020-01-01')) %>% ggplot(aes(x = Date, y = Volume)) + geom_point() + scale_y_log10()
+stock_monthly %>% filter(Date <= as.Date('2020-01-01')) %>% ggplot(aes(x = Date, y = Volume, col = High)) + geom_point() + scale_y_log10()
 
