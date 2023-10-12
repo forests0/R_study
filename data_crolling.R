b67b8ea0db2aef67daa82abcad8d913a48b13ff6
Chell2358 <forestser00@gmail.com>
2023-10-13 08:11:11 +0900

    .

--- data_crolling.R
+++ data_crolling.R
@@ -2,9 +2,13 @@ install.packages('quantmod')
 
 library(quantmod)
 
-getSymbols('AAPL')
 
 url.aapl = "https://www.quandl.com/api/v3/datasets/WIKI/AAPL/data.csv?api_key=xw3NU3xLUZ7vZgrz5QnG"
 data.aapl = read.csv(url.aapl)
 
-head(data.aapl)
+#head(data.aapl)
+
+getSymbols("AAPL", env=NULL)[2324:2328,]
+
+ticker = c('FB', 'NVDA') 
+getSymbols(ticker)
