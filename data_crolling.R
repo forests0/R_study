05d3f1b6f1b9839f5ef4dc8c201e267ba5700ff6
Chell2358 <forestser00@gmail.com>
2023-10-16 18:29:35 +0900

    .

--- data_crolling.R
+++ data_crolling.R
@@ -2,11 +2,10 @@ install.packages('quantmod')
 
 library(quantmod)
 
-
+#quandi의 데이터를 크롤링함
 url.aapl = "https://www.quandl.com/api/v3/datasets/WIKI/AAPL/data.csv?api_key=xw3NU3xLUZ7vZgrz5QnG"
 data.aapl = read.csv(url.aapl)
 
-#head(data.aapl)
 
 getSymbols("AAPL", env=NULL)[2324:2328,]
 
